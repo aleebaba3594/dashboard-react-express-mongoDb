9a62e4a1f9c88fdd210305d73622ce356a64c746
Ali Ahmed <aliofficial3594@gmail.com>
2023-03-26 22:37:04 +0500

    added client/build

--- index.js
+++ index.js
@@ -63,12 +63,12 @@ mongoose
   })
   .catch((error) => console.log(`${error} did not connect`));
 
-  
+
 //to deploy
   if (process.env.NODE_ENV !== "development") {
-app.use(express.static(path.join(__dirname,"./frontend/build")))
+app.use(express.static(path.join(__dirname,"./client/build")))
 
 app.get('*',(req, res) => {
-  res.sendFile(path.join(__dirname,"frontend/build/index.html"))
+  res.sendFile(path.join(__dirname,"client/build/index.html"))
 })
 }
